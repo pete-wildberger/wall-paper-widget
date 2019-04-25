3810b41e50c2e42eebd04b6b70a449ea5cc5c6e2
Peter Wildberger <pete.wildberger@gmail.com>
2019-04-24 21:48:30 -0500

    progress

--- main.ts
+++ main.ts
@@ -4,37 +4,61 @@ window.onload = function() {
 };
 
 class WallPaper {
-  modal: HTMLElement;
-  btn: HTMLElement;
-  span: HTMLElement;
+  $modal: HTMLElement;
+  $btn: HTMLElement;
+  $span: HTMLElement;
+  $addRows: Array<Element>;
 
   constructor() {
     // Get the modal
-    this.modal = document.getElementById('myModal') as HTMLElement;
-
+    this.$modal = document.getElementById('myModal') as HTMLElement;
     // Get the button that opens the modal
-    this.btn = document.getElementById('myBtn') as HTMLElement;
-
+    this.$btn = document.getElementById('myBtn') as HTMLElement;
     // Get the <span> element that closes the modal
-    this.span = document.getElementsByClassName('close')[0] as HTMLElement;
+    this.$span = document.getElementsByClassName('close')[0] as HTMLElement;
+    this.$addRows = Array.from(document.getElementsByClassName('addRow'));
   }
   init = () => {
     console.log('App start');
     // When the user clicks on the button, open the modal
-    this.btn.onclick = () => {
-      this.modal.style.display = 'block';
+    this.$btn.onclick = () => {
+      this.$modal.style.display = 'block';
     };
-
     // When the user clicks on <span> (x), close the modal
-    this.span.onclick = () => {
-      this.modal.style.display = 'none';
+    this.$span.onclick = () => {
+      this.$modal.style.display = 'none';
     };
-
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = event => {
-      if (event.target == this.modal) {
-        this.modal.style.display = 'none';
+      if (event.target == this.$modal) {
+        this.$modal.style.display = 'none';
       }
     };
+    this.$addRows.forEach((elem, i) => {
+      const el = <Element>elem.parentElement;
+      const id: string = el.getAttributeNode('id')!.value;
+      elem.addEventListener('click', () => {
+        this.addRow(id, i);
+      });
+    });
   };
+  addRow(parentDiv: string, id: number): void {
+    const row = document.createElement('div');
+    const rm = document.createElement('button');
+    rm.setAttribute('onclick', `removeElement('${parentDiv}','id_${id}')`);
+    row.setAttribute('class', 'row');
+    document.getElementById(parentDiv)!.appendChild(row);
+  }
+  removeElement(parentDiv: string, childDiv: string): void {
+    if (childDiv === parentDiv) {
+      alert('The parent div cannot be removed.');
+    } else if (document.getElementById(childDiv)) {
+      const child = document.getElementById(childDiv) as HTMLElement;
+      const parent = document.getElementById(parentDiv) as HTMLElement;
+      parent.removeChild(child);
+    } else {
+      alert('Child div has already been removed or does not exist.');
+      return;
+    }
+  }
 }
